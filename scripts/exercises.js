594c87b56cb9b7787cab08cf917c05acd78e28e9
Goran M <goranmuminovic@gmail.com>
2024-03-26 02:39:50 +0100

    saving

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,16 +1,14 @@
-const date = new Date;
-const adate = new Date;
+// Date
+const date = new Date();
+const adate = new Date();
 
-//Cannot redeclare block-scoped variable 'date3'.ts(2451)
-//const date3: Date
-
-console.log(date)
+console.log(date);
 console.log(date.toLocaleDateString("sv-SE"));
 console.log(date.toLocaleDateString("en-US"));
 console.log(date.toLocaleDateString());
 console.log(date.toTimeString());
 
-let date2 = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`
+let date2 = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
 console.log(date2);
 
 console.log(adate);
@@ -20,30 +18,56 @@ console.log(adate.getDate());
 
 console.log(adate.toISOString());
 
-console.log(date.toISOString().substring(0,10));
+console.log(date.toISOString().substring(0, 10));
 
 //document.getElementById("main").innerHTML = "Lexicon JavaScript Exercises.";
-
 //document.getElementById("date").innerHTML = date.toLocaleDateString("sv-SE");
 
-
-function isLeapYear(year){
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+//Leap year
+function isLeapYear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 }
 
 console.log(isLeapYear(2024));
 
-function celsiusToFahrenheit(degree){
-    return ((9/5*degree)+32);
+
+// Celsius to Fahrenheit conversion
+function celsiusToFahrenheit(degree) {
+  return (9 / 5) * degree + 32;
 }
-function fahrenheitToCelsius(degree){
-    return ((degree-32)*5/9);
+
+// Fahrenheit to Celsius conversion
+function fahrenheitToCelsius(degree) {
+  return ((degree - 32) * 5) / 9;
 }
 console.log(celsiusToFahrenheit(20));
 console.log(fahrenheitToCelsius(68));
 console.log(celsiusToFahrenheit(60));
 console.log(fahrenheitToCelsius(45));
 
-let text = "Some text";
-console.log(text.slice(0,3));
 
+// Remove text from a string
+let text = "Some text";
+console.log(text.slice(0, 3));
+
+// For of loop
+for (let i of text) {
+  console.log(i);
+}
+// For in loop
+for (let i in text) {
+  console.log(i);
+}
+
+// reverse a given string
+for (let i = text.length; i >= 0; i--) {
+  console.log(text[i]);
+}
+
+// Undefined
+for (let i of text) {
+  console.log(text[i]);
+}
+
+
+//console.log(text.length);
