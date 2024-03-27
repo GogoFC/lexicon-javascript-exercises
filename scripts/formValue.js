77b6785a62a197d2c7f50ab2fe7f173ea957c685
Goran M <goranmuminovic@gmail.com>
2024-03-27 02:52:09 +0100

    saving

---
+++ scripts/formValue.js
@@ -0,0 +1,11 @@
+
+  function getFormvalue() {
+
+    var form = document.forms[0];
+    var fName = form.querySelector('input[name="fname"]');
+    var firstName = fName.value;
+    var lName = form.querySelector('input[name="lname"]');
+    var lastName = lName.value;
+    console.log(firstName);
+
+  }
