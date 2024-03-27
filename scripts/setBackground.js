e2f2339181b86c949faff33dae580fb235a4aacd
Goran M <goranmuminovic@gmail.com>
2024-03-27 05:13:16 +0100

    colors

---
+++ scripts/setBackground.js
@@ -0,0 +1,15 @@
+function set_background(){
+    document.body.style.backgroundColor = "black";
+    document.getElementById("z1").style.backgroundColor = "yellow";
+    document.getElementById("z2").style.backgroundColor = "blue";
+    document.getElementById("z2").style.color = "yellow";
+    document.getElementById("z1").style.color = "black";
+}
+
+function reset_background(){
+    document.body.style.backgroundColor = "rgb(23, 20, 33)";
+
+}
+
+
+
